97184cc17d48cfbf19479dca9fa6fdc034f8ba8e
nghiaht <nghiahoangtrong@vietbando.vn>
2022-06-21 16:45:44 +0700

    update

--- testMVC/testMVC/Controllers/HomeController.cs
+++ testMVC/testMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using testMVC.Models;
 
 namespace testMVC.Controllers
 {
@@ -27,13 +28,43 @@ namespace testMVC.Controllers
 
             return View();
         }
+
         [HttpPost]
-        [ActionName("Complex")]
-        public ActionResult GetData(int customerID, string fname = "")
+        public JsonResult AjaxPostCall(MapModel mapdata)
         {
-            JsonResult result = new JsonResult();
-            var userName = "John";
-            return result;
+            MapModel map = new MapModel
+            {
+                Province = mapdata.Province,
+                Ward = mapdata.Ward,
+                soHieuToBanDo = mapdata.soHieuToBanDo,
+                soThuTuThua = mapdata.soThuTuThua
+            };
+
+            //if (map.soHieuToBanDo == 1 && map.Province == "tanphu" && map.Ward == "tanphu" && map.soThuTuThua == 25)
+            //{
+            MapModel data = new MapModel
+            {
+                soThuTuThua = 25,
+                soHieuToBanDo = 1,
+                maXa = "29461",
+                maLoaiDat = "abc",
+                dienTich = 927.3,
+                trangThaiDangKy = 2,
+                geo = "{\"type\"=\"Polygon\",\"coordinates\"=[[[106.257124560784,9.698294686187],[106.257145796504,9.69829608588],[106.257144226527,9.698164990785],[106.257335166298,9.698177859603],[106.257334933308,9.698155256919],[106.257195941108,9.698144714483],[106.257198229434,9.698108634719],[106.257334487622,9.698119092784],[106.257332248036,9.69793329943],[106.257354445343,9.697713547278],[106.257180448209,9.697738071923],[106.257124560784,9.698294686187]]]}"
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+            //}
+            //else
+            //{
+            //    return null;
+            //}
         }
+        [ChildActionOnly]
+        public PartialViewResult Result()
+        {
+            return PartialView("Result");
+        }
+
     }
 }
---
+++ testMVC/testMVC/Models/MapModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testMVC.Models
+{
+    public class MapModel
+    {
+        public string Province
+        {
+            get;
+            set;
+        }
+        public string Ward
+        {
+            get;
+            set;
+        }
+        public int soThuTuThua
+        {
+            get;
+            set;
+        }
+        public int soHieuToBanDo
+        {
+            get;
+            set;
+        }
+        public string maXa
+        {
+            get;
+            set;
+        }
+
+        public int trangThaiDangKy
+        {
+            get;
+            set;
+        }
+        public string maLoaiDat
+        {
+            get;
+            set;
+        }
+        public double dienTich
+        {
+            get;
+            set;
+        }
+        public string geo
+        {
+            get;
+            set;
+        }
+
+    }
+}

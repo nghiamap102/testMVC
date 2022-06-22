005338bc06637af684f0b5fd4e85b4b4e6a01760
nghiaht <nghiahoangtrong@vietbando.vn>
2022-06-22 16:57:21 +0700

    update

--- testMVC/testMVC/Controllers/HomeController.cs
+++ testMVC/testMVC/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace testMVC.Controllers
     {
         public ActionResult Index()
         {
-
+            ViewBag.abc = "abc";
             return View();
         }
 
@@ -28,6 +28,10 @@ namespace testMVC.Controllers
 
             return View();
         }
+        public ActionResult DataTable()
+        {
+            return View();
+        }
 
         [HttpPost]
         public JsonResult AjaxPostCall(MapModel mapdata)
@@ -40,25 +44,25 @@ namespace testMVC.Controllers
                 soThuTuThua = mapdata.soThuTuThua
             };
 
-            //if (map.soHieuToBanDo == 1 && map.Province == "tanphu" && map.Ward == "tanphu" && map.soThuTuThua == 25)
-            //{
-            MapModel data = new MapModel
+            if (map.soHieuToBanDo == 1 && map.Province == "tanphu" && map.Ward == "tanphu" && map.soThuTuThua == 25)
             {
-                soThuTuThua = 25,
-                soHieuToBanDo = 1,
-                maXa = "29461",
-                maLoaiDat = "abc",
-                dienTich = 927.3,
-                trangThaiDangKy = 2,
-                geo = "{\"type\"=\"Polygon\",\"coordinates\"=[[[106.257124560784,9.698294686187],[106.257145796504,9.69829608588],[106.257144226527,9.698164990785],[106.257335166298,9.698177859603],[106.257334933308,9.698155256919],[106.257195941108,9.698144714483],[106.257198229434,9.698108634719],[106.257334487622,9.698119092784],[106.257332248036,9.69793329943],[106.257354445343,9.697713547278],[106.257180448209,9.697738071923],[106.257124560784,9.698294686187]]]}"
-            };
+                MapModel data = new MapModel
+                {
+                    soThuTuThua = 25,
+                    soHieuToBanDo = 1,
+                    maXa = "29461",
+                    maLoaiDat = "DT+CLN",
+                    dienTich = 927.3,
+                    trangThaiDangKy = 2,
+                    geo = "{\"type\"=\"Polygon\",\"coordinates\"=[[[106.257124560784,9.698294686187],[106.257145796504,9.69829608588],[106.257144226527,9.698164990785],[106.257335166298,9.698177859603],[106.257334933308,9.698155256919],[106.257195941108,9.698144714483],[106.257198229434,9.698108634719],[106.257334487622,9.698119092784],[106.257332248036,9.69793329943],[106.257354445343,9.697713547278],[106.257180448209,9.697738071923],[106.257124560784,9.698294686187]]]}"
+                };
 
-            return Json(data, JsonRequestBehavior.AllowGet);
-            //}
-            //else
-            //{
-            //    return null;
-            //}
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return null;
+            }
         }
         [ChildActionOnly]
         public PartialViewResult Result()
